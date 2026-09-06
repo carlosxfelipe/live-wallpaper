c9a77c4054e90f8ac4999cef554d27fc1c23d94a
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2026-09-06 09:45:21 -0300

    First commit

---
+++ LiveWallpaper/LiveWallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveWallpaperApp.swift
+//  LiveWallpaper
+//
+//  Created by Carlos Felipe Araújo on 06/09/26.
+//
+
+import SwiftUI
+
+@main
+struct LiveWallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
